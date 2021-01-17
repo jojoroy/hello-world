45549889659872557e2f728b9bf36596999e6f90
jojoroy <77570139+jojoroy@users.noreply.github.com>
2021-01-17 10:22:51 +0000

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * HelloWorld.java: Prints out the message "Hello World!" to the user, and
+ * exits.
+ *
+ * Author: D. Moxey
+ * Date:   7/1/10
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello MA117");
+    }
+}
